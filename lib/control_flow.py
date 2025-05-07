aa8c3e9f6f4dad9be53c0c336d3fa9fcbea62cd9
dennissammy77 <dennissammy77@gmail.com>
2025-05-07 08:26:22 +0300

    conditional staements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,61 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     # your code here
     pass
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature > 40 and temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"            
     pass
 
 def fizzbuzz(num):
     # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
     # your code here
+    operators = ['+', '-', '*', '/']
+    if operation not in operators:
+        print("Invalid operation!")
+        return None
+    elif operation == '+':
+        return add(num1, num2)
+    elif operation == '-':
+        return subtract(num1, num2)
+    elif operation == '*':
+        return multiply(num1, num2)
+    elif operation == '/':
+        return divide(num1, num2)
     pass
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def divide(a,b):
+    return a / b
+
+def multiply(a,b):
+    return a * b
